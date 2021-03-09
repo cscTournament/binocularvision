474eababb13356111839fc3685b8dad49d5da6dd
DanyaTania <77732696+DanyaTania@users.noreply.github.com>
2021-03-09 04:36:18 +0300

    Add files via upload

---
+++ src/main/java/by/gourianova/binocularvision/main/IndexServlet.java
@@ -0,0 +1,19 @@
+package by.gourianova.binocularvision.main;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class IndexServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        getServletContext().getRequestDispatcher("/index").forward(req, resp);
+
+       resp.sendRedirect(req.getContextPath() + "/index");
+    }
+}
---
+++ src/main/java/by/gourianova/binocularvision/main/MainServlet.java
@@ -0,0 +1,82 @@
+package by.gourianova.binocularvision.main;
+
+//import by.gourianova.binocularvision.Index;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/index")
+public class MainServlet extends HttpServlet {
+//Index index=new Index();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        Integer visitCounter = (Integer) session.getAttribute("visitCounter");
+        if (visitCounter == null) {
+            visitCounter = 1;
+        } else {
+            visitCounter++;
+
+        }
+
+
+        session.setAttribute("visitCounter", visitCounter);
+        String username = req.getParameter("username");
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("<html>");
+        out.write("<head>");
+        out.write("<title>Welcome servlet</title>");
+        out.write("</head>");
+        out.write("<body>");
+
+
+
+        if (username == null) {
+            //TODO: revise how on site
+          out.write("<center>"+"<img src=\"/images/bv.jpg\">"+"</center>");
+            out.write("<br><br><br><br>");
+            out.write("<center>"+"<h3>"+"Good day, Anonymous" + "<br>");
+            out.write("<br>"+"<h3>"+"<center>"+
+            "We represent the scope of medical apps for the training and rehabilitation of disorders of binocular"+
+                   "and monocular vision"+
+                    "<br> (including  strabismus  amblyopia),<br>"+
+                    "prevention of these disorders in healthy individuals,"+
+                    "as well as developing apps for the stimulation of sensory-motor mechanisms and improve coordination and logical thinking. <br> Our apps are effectively used for children of preschool and school age"+
+                    "</center>"+"</h3>");
+
+            out.write("before you'll got an app or contact, you should register<br><br> " +
+                    "<h3>"+"<a href=\"Controller?command=gotoindexpage&message=Registration%20ok\">&Cscr;ontinue</a> "+"</h3>"+"</center>");
+
+
+        } else {
+            out.write("Good day, " + username + "<br>");
+            out.write("Page was visited " +visitCounter + " times.");
+
+
+
+            //TODO: to correct number of loading  this page
+            //TODO: to stop application after 3 wrong entering of password
+        }
+
+
+
+
+
+        out.write("</body>");
+        out.write("</html>");
+        out.close();
+    }
+
+
+
+
+}
