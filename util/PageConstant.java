c87c65a2de6cb873e86c4286e08c809d3c464b9f
DanyaTania <77732696+DanyaTania@users.noreply.github.com>
2021-03-15 13:15:37 +0300

    Add files via upload

---
+++ util/PageConstant.java
@@ -0,0 +1,18 @@
+package by.gourianova.binocularvision.util;
+
+
+public class PageConstant {
+    public final static String ERROR_PAGE = "jsp/error_page.jsp";
+    public final static String MAIN_PAGE = "jsp/main_page.jsp";
+    public final static String FIRST_PAGE = "index.jsp";
+    public final static String REGISTER_PAGE = "jsp/registration.jsp";
+    public final static String LOGIN_PAGE = "jsp/user_login.jsp";
+
+//TODO: write:
+  //  public final static String ALL_USERS = "includeAdmin/user_show_all.jsp";
+  //  public final static String UPDATE_USER = "includeAdmin/user_change.jsp";
+  // public final static String UPDATE_TYPE = "includeAdmin/type_change.jsp";
+ //   public final static String ONE_USER = "includeAdmin/user_show_one.jsp";
+
+
+}
