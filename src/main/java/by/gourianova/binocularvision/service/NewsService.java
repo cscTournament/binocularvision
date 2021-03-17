1eba73ef16328bfc21fc0e280dc4bf1e2713510f
DanyaTania <77732696+DanyaTania@users.noreply.github.com>
2021-03-17 16:36:27 +0300

    Delete NewsService.java

--- src/main/java/by/gourianova/binocularvision/service/NewsService.java
+++
@@ -1,9 +0,0 @@
-package by.gourianova.binocularvision.service;
-
-import by.gourianova.binocularvision.bean.News;
-
-import java.util.List;
-
-public interface NewsService {
-	List<News> takeAll() throws ServiceException;
-}
