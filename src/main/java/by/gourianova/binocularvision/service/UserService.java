0376fab6863ade1e96b4b184d0f41fa0bdd8b8d4
DanyaTania <77732696+DanyaTania@users.noreply.github.com>
2021-03-17 16:40:09 +0300

    Add files via upload

--- src/main/java/by/gourianova/binocularvision/service/UserService.java
+++ src/main/java/by/gourianova/binocularvision/service/UserService.java
@@ -3,7 +3,12 @@ package by.gourianova.binocularvision.service;
 import by.gourianova.binocularvision.bean.RegistrationInfo;
 import by.gourianova.binocularvision.bean.User;
 
+import java.util.ArrayList;
+
 public interface UserService {
 	User authorization(String login, String passport) throws ServiceException;
+
+	ArrayList<User> takeAll() throws ServiceException;
+
 	boolean registration(RegistrationInfo regInfo) throws ServiceException;
 }
